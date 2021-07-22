29f6f898109ba5ee0d17cfad6669cc0bd13e5b46
olaolasanoye <42591205+olaolasanoye@users.noreply.github.com>
2021-07-21 22:45:27 -0400

    created component structure

---
+++ my-shoppinglist-app/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ReceipeDetailComponent } from './recipes/receipe-detail/receipe-detail.component';
+import { ReceipeListComponent } from './recipes/receipe-list/receipe-list.component';
+import { ReceipeItemComponent } from './recipes/receipe-list/receipe-item/receipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ReceipeDetailComponent,
+    ReceipeListComponent,
+    ReceipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
